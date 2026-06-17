12955395ac569dfec5bb70b8649a49de8130a859
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-06-17 16:47:31 -0300

    Adicionado linhas de codigo no index.html e js/scripts_repeticoes.js referente ao assunto

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -51,3 +51,39 @@ for(i = 0;i < 5;i++ ){
     divArray.innerHTML += `${presentes[i]} <br>`
 }
 //FOR IN
+const divForIn = document.querySelector('#div-forin')
+
+for(let posicao in presentes){
+    divForIn.innerHTML += `${presentes[posicao]} <br>`
+}
+
+//FOR OF
+const divForOf = document.querySelector('#div-forof')
+
+for (let elemento of presentes){
+    divForOf.innerHTML += `${elemento} <br>`
+}
+
+//FOREACH
+const divForeach = document.querySelector('#div-foreach')
+
+presentes.forEach((elemento, posicao)=>{
+    divForeach.innerHTML += `${posicao + 1} - ${elemento} <br>`
+})
+
+//COLEÇÃO DE OBJETOS LITERAIS - ARRAY
+const pessoas = [
+    {nome: 'Maria Flor', idade: 25, renda:8500},
+    {nome: 'Joerdson Souza', idade: 75, renda:5000},
+    {nome: 'Taoca', idade: 28, renda:500},
+    {nome: 'Chicó', idade: 36, renda:100},
+    {nome: 'João Grilo', idade: 32, renda:80},    
+]
+
+//LISTANDO OBJETO LITERAL PELO FOR
+const divListaObjFor = document.querySelector('#div-listaobj-for')
+
+for(i= 0; i < pessoas.length;i++){
+    divListaObjFor.innerHTML += `${pessoas[i].nome}, ${pessoas[i]}.idade} R$ ${pessoas[i].renda.toFixed(2).replace('.', ',')} <br>`
+
+}
