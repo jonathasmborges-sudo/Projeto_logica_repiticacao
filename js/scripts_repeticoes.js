3d6eedb2bd3091a8b8678497ef76a2891162f12e
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-06-17 14:27:58 -0300

    Corrijido alguns erros na linha de codigo do index.html e js/scripts_repeticoes.js

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -3,12 +3,12 @@ const divFor= document.querySelector('#div-for')
 
 for(i = 0;i < 10; i++){
     console.log (i, 'Amor')
-    divFor.innerHTML += '$(i) - Amor <br>'
+    divFor.innerHTML += `${i} - Amor <br> `
 }
 
 //Contador/Acumulador
 const inputNum = document.querySelector('#num')
-const btnNum = document.querySelector('btn-num')
+const btnNum = document.querySelector('#btn-num')
 const divContAcum = document.querySelector('#div-cont-acum')
 
 let cont = 0
@@ -20,7 +20,24 @@ btnNum.addEventListener('click', (evt)=>{
  cont++ 
  acum += numDigitado
 
- divContAcum.innerHTML = 'Total de Número Digitado é: ${cont} <br> A soma dos Números é'
- inputNum.value = ''
+ divContAcum.innerHTML = `Total de números digitados: ${cont} <br>
+    Soma dos números: ${acum}`
 
 })
+
+//CONTROLANDO FOR 
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const bntFrase = document.querySelector('#bnt-frase')
+const divFrase = document.querySelector('#div-frase')
+
+bntFrase.addEventListener('click', (evt) => {
+    let numRepeti = Number(inputNumRepeticao.value)
+    let frase = inputFrase.value
+
+    for (i = 0; i < numRepeti; i++){
+        divFrase.innerHTML += `${i + 1} - ${frase} <br>`
+    }
+})
+
+//FOR IN
